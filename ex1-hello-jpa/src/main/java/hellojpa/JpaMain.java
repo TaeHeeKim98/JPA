a5c1bdc4aedf16ca24479e2b7c5112728859f0af
TaeHeeKim <eksrnsdhkdwk@gmail.com>
2024-03-09 20:22:00 +0900

    no message

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,13 +15,13 @@ public class JpaMain {
 
         try {
 
-            //영속
-            Member member = em.find(Member.class, 150L);
-            member.setName("AAAAA");
+            Member member = new Member();
+            member.setId(3L);
+            member.setUsername("C");
+            member.setRoleType(RoleType.GUEST);
+            em.persist(member);
 
-            em.clear();
 
-            System.out.println("=========================");
             tx.commit(); // 트랜젝션 커밋
         } catch (Exception e) {
             tx.rollback();
